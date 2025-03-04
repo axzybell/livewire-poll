8727e37144e10b92dc3c77eb1a05aa06d22c334d
Chong Jun Wei <chongjw001160@gmail.com>
2025-03-04 17:41:46 +0800

    Commit - Added Livewire CreatePoll Component - Added create-poll blade - Modified app.blade directive to use create-poll

---
+++ resources/views/livewire/create-poll.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <form action="">
+        <label for="">Poll Title</label>
+
+        <input type="text" wire:model.live="title"/>
+
+        Current Title: {{ $title }}
+    </form>
+</div>
