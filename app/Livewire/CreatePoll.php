3b4a3c98961da122250a12bc9b3622623b72d352
Chong Jun Wei <chongjw001160@gmail.com>
2025-03-06 21:48:24 +0800

    Changes: - Added Creating a Poll - Added Editing Poll Options

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -2,13 +2,45 @@
 
 namespace App\Livewire;
 
+use App\Models\Poll;
 use Livewire\Component;
 
 class CreatePoll extends Component
 {
     public $title;
+    public $options = ['First'];
+
     public function render()
     {
         return view('livewire.create-poll');
+    }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        $this->options = array_values($this->options);
+    }
+
+    public function createPoll()
+    {
+
+        Poll::create([
+            'title' => $this->title,
+        ])->options()->createMany(
+            collect($this->options)
+                ->map(fn ($option) => ['name' => $option])
+                ->all()
+        );
+
+        // foreach($this->options as $optionName) {
+        //     $poll->options()->create(['name' => $optionName]);
+        // }
+
+        $this->reset(['title', 'options']);
     }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,9 +1,27 @@
 <div>
-    <form action="">
+    <form action="" wire:submit.prevent="createPoll">
         <label for="">Poll Title</label>
 
         <input type="text" wire:model.live="title"/>
 
-        Current Title: {{ $title }}
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
+
+        <div>
+            @foreach ($options as $index => $option)
+            <div class="mb-4">
+                <label for="">Option {{ $index + 1 }}</label>
+
+                <div class="flex gap-2">
+                    <input type="text" wire:model="options.{{ $index }}"/>
+                    <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+        <button type="submit" class="btn">Create Poll</button>
+
     </form>
 </div>
